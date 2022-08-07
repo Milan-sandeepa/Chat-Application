c3fdf333bc4adfe201b6c186c164108ab34720a7
Milan-sandeepa <glmsandeepa@gmail.com>
2022-08-07 13:18:38 +0530

    Runnable methods implemented

--- src/ServerApp.java
+++ src/ServerApp.java
@@ -1,10 +1,14 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashSet;
 
 public class ServerApp {
+    private static final int PORT=8000;
+    private static HashSet<String>names = new HashSet<>();
+    private static HashSet<PrintWriter>writers=new HashSet<>();
+
     public static void main(String[] args) {
-        final int PORT=8000;
         try {
             ServerSocket serverSocket = new ServerSocket(PORT);
             System.out.println("Server is running in port: " + PORT);
@@ -14,6 +18,67 @@ public class ServerApp {
 
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    private static class Handler implements Runnable{
+
+        private String name;
+        private Socket socket;
+        private BufferedReader in;
+        private PrintWriter out;
+
+        public Handler(Socket socket){
+            this.socket=socket;
+        }
+
+        @Override
+        public void run() {
+            try {
+                in=new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                out=new PrintWriter(socket.getOutputStream(),true);
+
+                while (true){
+                    out.println("SUBMINAME");
+                    name= in.readLine();
+
+                    if (name == null){
+                        return;
+                    }
+                    if (!names.contains(name)){
+                        names.add(name);
+                        break;
+                    }
+                }
+                out.println("NAMEACCEPTED");
+                writers.add(out);
+
+                while (true){
+                    String input = in.readLine();
+                    if (input == null){
+                        return;
+                    }
+                    for (PrintWriter writer :writers){
+                        writer.println("MESSAGE" + name + ": " + input);
+                    }
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }finally {
+                if (names!= null){
+                    names.remove(name);
+                }
+                if (out!=null){
+                    writers.remove(out);
+                }
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
         }
     }
 }
--- src/controller/Client.java
+++ src/controller/Client.java
@@ -4,7 +4,9 @@ import java.io.IOException;
 import java.net.Socket;
 
 public class Client {
+
     public static void main(String[] args) {
+
         try {
             Socket socket = new Socket("localhost",8000);
         } catch (IOException e) {
