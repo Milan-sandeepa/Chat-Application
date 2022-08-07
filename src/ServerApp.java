de096f18d71e018cac7005a18c4dd0ebeeaa496b
Milan-sandeepa <glmsandeepa@gmail.com>
2022-08-07 12:02:03 +0530

    serverSocket created

--- src/ServerApp.java
+++ src/ServerApp.java
@@ -1,2 +1,19 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ServerApp {
+    public static void main(String[] args) {
+        final int PORT=8000;
+        try {
+            ServerSocket serverSocket = new ServerSocket(PORT);
+            System.out.println("Server is running in port: " + PORT);
+
+            Socket localSocket = serverSocket.accept();
+            System.out.println("Client accepted..!");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
---
+++ src/controller/Client.java
@@ -0,0 +1,14 @@
+package controller;
+
+import java.io.IOException;
+import java.net.Socket;
+
+public class Client {
+    public static void main(String[] args) {
+        try {
+            Socket socket = new Socket("localhost",8000);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
